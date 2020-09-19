d4a35d2a6b71fbcfa6519f1668ec41f1a0745b6d
KitaharaMugiro <you@example.com>
2020-09-19 09:55:40 +0900

    SDL first development

--- src/server.ts
+++ src/server.ts
@@ -1,63 +1,44 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga'
+import { Message, Resolvers } from "./generated/graphql"
 
-const messages = [];
+const messages: Message[] = [];
 const subscribers = [];
 const onMessagesUpdates = (fn) => subscribers.push(fn);
 
-const typeDefs = `
-  type Message {
-    id: ID!
-    user: String!
-    content: String!
-  }
-
-  type Query {
-    messages: [Message!]
-  }
-  
-  type Mutation {
-    postMessage(user: String!, content: String!): ID!
-  }
-  
-  type Subscription {
-    messages: [Message!]
-  }
-`
-
-const resolvers = {
-
+const resolvers: Resolvers = {
   Query: {
-    messages: () => messages,
+    messages: () => messages
   },
 
   Mutation: {
-    postMessage: (parent, { user, content }) => {
-      const id = messages.length;
+    postMessage: (parent, args) => {
+      const { user, content } = args
+      const id = messages.length.toString()
       messages.push({
         id,
-        user,
         content,
-      });
-      subscribers.forEach((fn) => fn());
-      return id;
-    },
+        user
+      })
+      subscribers.forEach((call) => call())
+      return id
+    }
   },
-
   Subscription: {
     messages: {
       subscribe: (parent, args, { pubsub }) => {
-        const channel = "test2";
+        const channel = "test";
         onMessagesUpdates(() => pubsub.publish(channel, { messages }));
         setTimeout(() => pubsub.publish(channel, { messages }), 0);
-        return pubsub.asyncIterator(channel);
-      },
-    },
-  },
+        return pubsub.asyncIterator(channel)
+      }
+    }
+  }
+
 
 };
 
 const pubsub = new PubSub();
-const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
+const server = new GraphQLServer({ typeDefs: ["./src/graphql/message.graphql"], resolvers, context: { pubsub } });
 server.start(({ port }) => {
   console.log(`Server on http://localhost:${port}/`);
 });
